9f41ce02eb68761fcf23e008f8faca5821e71127
jaquelinekkkkk <jaqueline.c2004@aluno.ifsc.edu.br>
2025-06-30 01:20:26 -0300

    Novas telas e DAO's fluxo de projeto e campus +- completo falta imagens,

--- model/projetosDAO.php
+++ model/projetosDAO.php
@@ -15,19 +15,89 @@ function buscarProjetosPorCampus($idCampus) {
         LEFT JOIN academicos.areas_projetos ap ON ap.idProjeto = p.idProjeto
         LEFT JOIN academicos.areasdeconhecimento a ON a.idArea = ap.idArea
         WHERE p.idCampus = :idCampus";
-    // $sql = "SELECT p.*, 
-    //                u.nome AS nomeCoordenador, 
-    //                f.arquivoFoto
-    //         FROM academicos.projetos p
-    //         JOIN academicos.coordenadores_projetos cp ON cp.idProjeto = p.idProjeto
-    //         JOIN academicos.coordenadores c ON c.idUsuario = cp.idUsuario
-    //         JOIN academicos.usuarios u ON u.idUsuario = c.idUsuario
-    //         LEFT JOIN academicos.fotos_perfil_projeto f ON f.idProjeto = p.idProjeto
-    //         WHERE p.idCampus = :idCampus";
+   
 
     $pdo = conectar();
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':idCampus', $idCampus);
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function buscarDetalhesProjeto($idProjeto) {
+    $sql = "SELECT 
+                p.idProjeto,
+                p.tituloProjeto,
+                p.resumo,
+                p.edital,
+                p.dataInicio,
+                p.dataFim,
+                p.prorrogacao,
+                p.emAndamento,
+                c.nomeCampus AS nomeCampus,
+                f.arquivoFoto,
+                a.nomeArea
+            FROM academicos.projetos p
+            JOIN academicos.campus c ON c.idCampus = p.idCampus
+            LEFT JOIN academicos.fotos_perfil_projeto f ON f.idProjeto = p.idProjeto
+            LEFT JOIN academicos.areas_projetos ap ON ap.idProjeto = p.idProjeto
+            LEFT JOIN academicos.areasdeconhecimento a ON a.idArea = ap.idArea
+            WHERE p.idProjeto = :idProjeto";
+
+    $pdo = conectar();
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':idProjeto', $idProjeto, PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+function buscarCoordenadoresProjeto($idProjeto) {
+    $sql = "SELECT u.nome 
+            FROM academicos.coordenadores_projetos cp
+            JOIN academicos.usuarios u ON u.idUsuario = cp.idUsuario
+            WHERE cp.idProjeto = :idProjeto";
+
+    $pdo = conectar();
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':idProjeto', $idProjeto, PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_COLUMN);
+}
+
+
+function buscarBolsistasProjeto($idProjeto) {
+    $sql = "SELECT u.nome 
+            FROM academicos.bolsistas_projetos bp
+            JOIN academicos.usuarios u ON u.idUsuario = bp.idUsuario
+            WHERE bp.idProjeto = :idProjeto";
+
+    $pdo = conectar();
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':idProjeto', $idProjeto, PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_COLUMN);
+}
+
+function buscarPostagensProjeto($idProjeto) {
+    $sql = "SELECT legenda 
+            FROM academicos.postagens 
+            WHERE idProjeto = :idProjeto";
+
+    $pdo = conectar();
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':idProjeto', $idProjeto, PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_COLUMN);
+}
+
+function buscarArtigosProjeto($idProjeto) {
+    $sql = "SELECT titulo 
+            FROM academicos.artigos 
+            WHERE idProjeto = :idProjeto";
+
+    $pdo = conectar();
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':idProjeto', $idProjeto, PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_COLUMN);
 }
